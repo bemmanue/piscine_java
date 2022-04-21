c196c2b225ab88d1bf01775afe60bc2c87e034fc
Burma emmanuetestbs <bemmanuetestbs@ev-r1.kzn.21-school.ru>
2022-04-21 22:46:01 +0300

    Completed

---
+++ day03/ex00/src/Program.java
@@ -0,0 +1,38 @@
+public class Program {
+
+    public static void main(String[] args) {
+        int count = 0;
+        if (args.length != 1 || !args[0].startsWith("--count=")) {
+            System.out.println("Wrong argument");
+            System.exit(-1);
+        } else {
+            try {
+                count = Integer.parseInt(args[0].substring(8));
+            }
+            catch (Exception e) {
+                System.out.println("Wrong argument");
+            }
+        }
+
+        Runnable hen_task = new MyRunnable(count, "Hen");
+        Runnable egg_task = new MyRunnable(count, "Egg");
+
+        Thread egg = new Thread(egg_task);
+        Thread hen = new Thread(hen_task);
+
+        egg.start();
+        hen.start();
+
+        try {
+            egg.join();
+            hen.join();
+        }
+        catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+
+        for (int i = 0; i < count; i++) {
+            System.out.println("Human");
+        }
+    }
+}
